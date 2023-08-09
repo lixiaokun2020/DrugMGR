cdbe3b3a1c3be1b212950b9237c63e201fc8e5e0
lixiaokun2020 <59496449+lixiaokun2020@users.noreply.github.com>
2023-08-09 15:58:52 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,245 @@
+import os
+import numpy as np
+from math import sqrt
+from scipy import stats
+from torch_geometric.data import InMemoryDataset, DataLoader
+from torch_geometric import data as DATA
+import torch
+import codecs
+from subword_nmt.apply_bpe import BPE
+import pandas as pd
+
+
+
+
+vocab_path = 'data/ESPF/protein_codes_uniprot.txt'
+bpe_codes_protein = codecs.open(vocab_path)
+pbpe = BPE(bpe_codes_protein, merges=-1, separator='')
+sub_csv = pd.read_csv('data/ESPF/subword_units_map_uniprot.csv')
+
+
+idx2word_p = sub_csv['index'].values
+words2idx_p = dict(zip(idx2word_p, range(0, len(idx2word_p))))
+
+vocab_path = 'data/ESPF/drug_codes_chembl.txt'
+bpe_codes_drug = codecs.open(vocab_path)
+dbpe = BPE(bpe_codes_drug, merges=-1, separator='')
+sub_csv = pd.read_csv('data/ESPF/subword_units_map_chembl.csv')
+
+idx2word_d = sub_csv['index'].values
+words2idx_d = dict(zip(idx2word_d, range(0, len(idx2word_d))))
+
+max_seq_len = 1000
+max_smi_len = 100
+
+thre = 16640
+
+
+
+
+CHARISOSMISET = {"#": 29, "%": 30, ")": 31, "(": 1, "+": 32, "-": 33, "/": 34, ".": 2,
+                 "1": 35, "0": 3, "3": 36, "2": 4, "5": 37, "4": 5, "7": 38, "6": 6,
+                 "9": 39, "8": 7, "=": 40, "A": 41, "@": 8, "C": 42, "B": 9, "E": 43,
+                 "D": 10, "G": 44, "F": 11, "I": 45, "H": 12, "K": 46, "M": 47, "L": 13,
+                 "O": 48, "N": 14, "P": 15, "S": 49, "R": 16, "U": 50, "T": 17, "W": 51,
+                 "V": 18, "Y": 52, "[": 53, "Z": 19, "]": 54, "\\": 20, "a": 55, "c": 56,
+                 "b": 21, "e": 57, "d": 22, "g": 58, "f": 23, "i": 59, "h": 24, "m": 60,
+                 "l": 25, "o": 61, "n": 26, "s": 62, "r": 27, "u": 63, "t": 28, "y": 64}
+
+CHARISOSMILEN = 64
+
+CHARPROTSET = {"A": 1, "C": 2, "B": 3, "E": 4, "D": 5, "G": 6,
+               "F": 7, "I": 8, "H": 9, "K": 10, "M": 11, "L": 12,
+               "O": 13, "N": 14, "Q": 15, "P": 16, "S": 17, "R": 18,
+               "U": 19, "T": 20, "W": 21,
+               "V": 22, "Y": 23, "X": 24,
+               "Z": 25, "J":26}
+
+CHARPROTLEN = 26
+
+
+
+
+def protein2emb_encoder(x):
+    max_p = 545
+    t1 = pbpe.process_line(x).split()  # split
+    try:
+        i1 = np.asarray([words2idx_p[i] for i in t1])  # index
+    except:
+        i1 = np.array([0])
+        # print(x)
+
+    l = len(i1)
+
+    if l < max_p:
+        i = np.pad(i1, (0, max_p - l), 'constant', constant_values=0)
+        input_mask = ([1] * l) + ([0] * (max_p - l))
+    else:
+        i = i1[:max_p]
+        input_mask = [1] * max_p
+
+    return i, np.asarray(input_mask)
+
+
+def drug2emb_encoder(x):
+    max_d = 50
+    # max_d = 100
+    t1 = dbpe.process_line(x).split()  # split
+    try:
+        i1 = np.asarray([words2idx_d[i] for i in t1])  # index
+    except:
+        i1 = np.array([0])
+        # print(x)
+
+    l = len(i1)
+
+    if l < max_d:
+        i = np.pad(i1, (0, max_d - l), 'constant', constant_values=0)
+        input_mask = ([1] * l) + ([0] * (max_d - l))
+
+
+    else:
+        i = i1[:max_d]
+        input_mask = [1] * max_d
+    return i, np.asarray(input_mask)
+
+
+def label_smiles(line, MAX_SMI_LEN, smi_ch_ind):
+    X = np.zeros(MAX_SMI_LEN)
+    for i, ch in enumerate(line[:MAX_SMI_LEN]):  # x, smi_ch_ind, y
+        X[i] = smi_ch_ind[ch]
+
+    return X
+
+
+
+class TestbedDataset(InMemoryDataset):
+    def __init__(self, root='/tmp', dataset='davis', 
+                 xd=None, xt=None, y=None, transform=None,
+                 pre_transform=None,smile_graph=None):
+
+        #root is required for save preprocessed data, default is '/tmp'
+        super(TestbedDataset, self).__init__(root, transform, pre_transform)
+        # benchmark dataset, default = 'davis'
+        self.dataset = dataset
+        if os.path.isfile(self.processed_paths[0]):
+            print('Pre-processed data found: {}, loading ...'.format(self.processed_paths[0]))
+            self.data, self.slices = torch.load(self.processed_paths[0])
+            print(self.data)
+        else:
+            print('Pre-processed data {} not found, doing pre-processing...'.format(self.processed_paths[0]))
+            self.process(xd, xt, y,smile_graph)
+            self.data, self.slices = torch.load(self.processed_paths[0])
+            print(self.data)
+
+    @property
+    def raw_file_names(self):
+        pass
+        #return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return [self.dataset + '.pt']
+
+    def download(self):
+        # Download to `self.raw_dir`.
+        pass
+
+    def _download(self):
+        pass
+
+    def _process(self):
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+
+    # Customize the process method to fit the task of drug-target affinity prediction
+    # Inputs:
+    # XD - list of SMILES, XT: list of encoded target (categorical or one-hot),
+    # Y: list of labels (i.e. affinity)
+    # Return: PyTorch-Geometric format processed data
+    def process(self, xd, xt, y,smile_graph):
+        assert (len(xd) == len(xt) and len(xt) == len(y)), "The three lists must be the same length!"
+        data_list = []
+        data_len = len(xd)
+        for i in range(data_len):
+            # print('Converting SMILES to graph: {}/{}'.format(i+1, data_len))
+            smiles = xd[i]
+            target = xt[i]
+            labels = y[i]
+
+            seq_xd = label_smiles(smiles,max_smi_len,CHARISOSMISET)
+
+            d_v, d_mask = drug2emb_encoder(smiles)
+            # convert SMILES to molecular representation using rdkit
+            c_size, features, edge_index = smile_graph[smiles]
+
+            # print("edge_index:", edge_index[0])
+            # print("edge_index:", len(edge_index))
+            # print("features:", len(features))
+
+
+            # for i in range(thre - len(features)):
+            #     features.append([0]*78)
+            #
+            # for i in range(thre - len(edge_index)):
+
+
+            # make the graph ready for PyTorch Geometrics GCN algorithms:
+            GCNData = DATA.Data(x=torch.Tensor(features),
+                                edge_index=torch.LongTensor(edge_index).transpose(1, 0),
+                                y=torch.FloatTensor([labels]))
+
+            GCNData.target = torch.LongTensor([target])
+            GCNData.seq_xd = torch.LongTensor([seq_xd])
+            GCNData.d_v = torch.LongTensor([d_v])
+            GCNData.d_mask = torch.LongTensor(d_mask)
+
+            GCNData.__setitem__('c_size', torch.LongTensor([c_size]))
+            # append graph, label and target sequence to data list
+            data_list.append(GCNData)
+
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+        print('Graph construction done. Saving to file.')
+        data, slices = self.collate(data_list)
+        # save preprocessed data:
+        torch.save((data, slices), self.processed_paths[0])
+
+def rmse(y,f):
+    rmse = sqrt(((y - f)**2).mean(axis=0))
+    return rmse
+def mse(y,f):
+    mse = ((y - f)**2).mean(axis=0)
+    return mse
+def pearson(y,f):
+    rp = np.corrcoef(y, f)[0,1]
+    return rp
+def spearman(y,f):
+    rs = stats.spearmanr(y, f)[0]
+    return rs
+def ci(y,f):
+    ind = np.argsort(y)
+    y = y[ind]
+    f = f[ind]
+    i = len(y)-1
+    j = i-1
+    z = 0.0
+    S = 0.0
+    while i > 0:
+        while j >= 0:
+            if y[i] > y[j]:
+                z = z+1
+                u = f[i] - f[j]
+                if u > 0:
+                    S = S + 1
+                elif u == 0:
+                    S = S + 0.5
+            j = j - 1
+        i = i - 1
+        j = i-1
+    ci = S/z
+    return ci
